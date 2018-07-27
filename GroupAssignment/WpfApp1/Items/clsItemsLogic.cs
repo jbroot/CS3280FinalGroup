55dcaec996e9629450c206122797b778476f85d4
jbroot <jarren.briscoe@gmail.com>
2018-07-27 08:49:28 -0600

    Add files via upload

---
+++ GroupAssignment/WpfApp1/Items/clsItemsLogic.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment
+{
+    public class clsItemsLogic
+    {
+        clsItemsSQL db;
+
+        public clsItemsLogic()
+        {
+            db = new clsItemsSQL();
+        }
+
+        /// <summary>
+        /// Adds an Item to the database
+        /// </summary>
+        /// <param name="pKey"></param>
+        /// <param name="ItemDesc"></param>
+        /// <param name="cost"></param>
+        /// <returns>Number of rows affected or -1 if the pKey is already used</returns>
+        public int AddItem(string pKey, string ItemDesc, double cost)
+        {
+            try
+            {
+                string result = db.ExecuteScalarSQL("SELECT * FROM ItemDesc WHERE ItemCode = '" + pKey + "';");
+                if (result == "")
+                {
+                    return db.ExecuteNonQuery("INSERT INTO ItemDesc (ItemCode, ItemDesc, Cost) VALUES('" + pKey + "','" + ItemDesc + "','" + cost.ToString() + "');");
+                }
+                else return -1;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// Deletes an ItemCode pKey from the database
+        /// </summary>
+        /// <param name="pKey"></param>
+        /// <returns>Number of rows affected</returns>
+        public int DeleteItem(string pKey)
+        {
+            try
+            {
+                return db.ExecuteNonQuery("DELETE FROM ItemDesc WHERE ItemCode = '" + pKey + "';");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// Deletes all items that match ItemDesc and cost
+        /// </summary>
+        /// <param name="ItemDesc"></param>
+        /// <param name="cost"></param>
+        /// <returns>Number of rows affected</returns>
+        public int DeleteItem(string ItemDesc, double cost)
+        {
+            try
+            {
+                return db.ExecuteNonQuery("DELETE FROM ItemDesc WHERE ItemDesc = '" + ItemDesc + "' AND cost = " + cost.ToString() + ";");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public int UpdateItem(string pKey, string ItemDesc, double cost)
+        {
+            try
+            {
+                return db.ExecuteNonQuery("UPDATE ItemDesc SET ItemDesc = '" + ItemDesc + "', Cost = '" + cost.ToString() + "' WHERE ItemCode='" + pKey + "';");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        public DataSet DisplayItemDescTable(ref int rows)
+        {
+            try
+            {
+                return db.ExecuteSQLStatement("SELECT * FROM ItemDesc ORDER BY ItemCode", ref rows);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+    }
+}
---
+++ GroupAssignment/WpfApp1/Items/wndItems.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupAssignment
+{
+    /// <summary>
+    /// Interaction logic for wndItems.xaml
+    /// </summary>
+    public partial class wndItems : Window
+    {
+        clsItemsLogic myLogic;
+        public wndItems()
+        {
+            InitializeComponent();
+            myLogic = new clsItemsLogic();
+
+            gui();
+
+            test();
+        }
+
+        void gui()
+        {
+            int rowsAffected = 0;
+            DataSet myData = myLogic.DisplayItemDescTable(ref rowsAffected);
+            ItemDescTableDataGrid.DataContext = myData;
+            
+        }
+
+
+        /// <summary>
+        /// Test Logic class. Delete before final project
+        /// </summary>
+        void test()
+        {
+            myLogic.AddItem("AA", "Test", 100.3553);
+            myLogic.UpdateItem("AA", "TestUpdate", 50);
+            myLogic.DeleteItem("AA");
+
+            if (myLogic.AddItem("A", "-1", 60) != -1) throw new Exception("-1 didn't return from used pKey");
+            myLogic.AddItem("AA", "TestOveride", 56);
+            myLogic.DeleteItem("TestOveride", 56);
+        }
+        
+    }
+}
