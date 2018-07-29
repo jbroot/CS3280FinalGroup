d754c57f1fe94542cbee79648cdbd82ac46da66e
ec3280 <41838773+ec3280@users.noreply.github.com>
2018-07-28 19:08:19 -0600

    ec version
    Main ec version

---
+++ GroupAssignment/GroupAssignment/Main/LineItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment.Main
+{
+    class LineItem
+    {
+        public Int32 InvoiceNum { get; set; }
+        public Int32 LineItemNum { get; set; }
+
+        public Items.Item item { get; set; }
+        //public String ItemCode { get; set; }
+
+
+
+    }
+}
